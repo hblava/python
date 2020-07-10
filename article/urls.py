20d47aa4c3b7921f4be122a2f32e66ee96350a20
lava <860145856@qq.com>
2020-07-10 17:46:57 +0800

    delete 功能

--- article/urls.py
+++ article/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     # 文章详情
     path('article-detail/<int:id>/',views.article_detail,name='article_detail'),
     # 写文章
-    path('article-create/',views.article_create,name='article_create')
+    path('article-create/',views.article_create,name='article_create'),
+    # 删除文章
+    path('article-delete/<int:id>/',views.article_delete,name='article_delete'),
+    # 安全删除文章
+    path('article-safe-delete/<int:id>/',views.article_safe_delete,name='article_safe_delete'),
 ]
--- article/views.py
+++ article/views.py
@@ -80,3 +80,20 @@ def article_create(request):
         context = {'article_post_form': article_post_form }
         # 返回模板
         return render(request,'article/create.html',context)
+
+
+# 删文章
+def article_delete(request,id):
+    article = ArticlePost.objects.get(id=id)
+    article.delete()
+    return redirect("article:article_list")
+
+
+## 安全删除文章
+def article_safe_delete(request,id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        article.delete()
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("仅允许post请求")
